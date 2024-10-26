26d044ecf8934a43bd28a8d41a11c387ba017303
Junxiang Lin <lin.junx@northeastern.edu>
2024-10-26 16:52:34 -0400

    added documentation to fear meter script

--- FearMeter/Scripts/fear_meter.gd
+++ FearMeter/Scripts/fear_meter.gd
@@ -3,10 +3,17 @@ extends ProgressBar
 @onready var timer = $Timer
 @onready var damage_bar = $DamageBar
 
+'''
+The Fear Bar representing how much fear the npc is experiencing. 
+Consists of a Fear Meter and a Damage Bar. 
+The damage bar ui increases when the fear increases. 
+The fear meter ui follows suit with a small delay. 
+Any decrease in fear results in an instant change for both the damage and fear bars. 
+'''
 
+var fear:float = 0 : set = _update_fear
 
-var fear = 0 : set = _update_fear
-
+# Initializes a fear bar with value 0 and a given max_value
 func init_fear_bar(_max:float):
 	value = 0
 	max_value = _max
@@ -27,5 +34,7 @@ func _update_fear(new_fear):
 	else:
 		value = fear
 
+# When the previously set timer reaches 0, 
+# the fear meter UI catches up with the damage bar
 func _on_timer_timeout():
 	value = fear
