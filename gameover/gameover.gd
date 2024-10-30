8dee5db52ef2ba07f7e729d6c00a242b532513b2
utkarshdev2 <awesomeutk@gmail.com>
2024-10-29 22:52:58 -0400

    deathscreen2

--- gameover/gameover.gd
+++ gameover/gameover.gd
@@ -1,6 +1,8 @@
 extends CanvasLayer
 
-
+func _ready() -> void:
+	PlayerManager.player.visible = false
+	
 func _on_restart_pressed():
 	#on restart, start a new game
 	get_tree().change_scene_to_file("res://Levels/Level_00/level_00.tscn")
