1b2f976fb2c7a81c7891221fa12c7af65e39ebbf
iokilo <jake672005@gmail.com>
2024-10-31 13:12:50 -0400

    Updated trash can falling anim

--- props/Trash/trash.gd
+++ props/Trash/trash.gd
@@ -1,12 +1,12 @@
 class_name Trash extends Node2D
 
+signal finished
+
 @onready var sprite_2d: Sprite2D = $Sprite2D
-@onready var sprite_2d_toppled: Sprite2D = $Sprite2D_toppled
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready():
 	$HitBox.Damaged.connect( TakeDamage )
-	sprite_2d_toppled.visible = false
 	pass
 	
 func TakeDamage( _damage : int ) -> void:
@@ -18,6 +18,4 @@ func topple() -> void:
 	animation_player.animation_finished.connect(_on_animation_finished)
 	
 func _on_animation_finished(name: String) -> void:
-	if name == "toppling":
-		sprite_2d.visible = false
-		sprite_2d_toppled.visible = true
+	finished.emit()
