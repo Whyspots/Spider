bce4a5fc0c84a58da2b30d800f7f5d041b5db4dc
utkarshdev2 <awesomeutk@gmail.com>
2024-10-31 12:03:11 -0400

    Added trash toppling on attack

--- props/Trash/trash.gd
+++ props/Trash/trash.gd
@@ -1,9 +1,23 @@
-class_name Plant extends Node2D
+class_name Trash extends Node2D
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var sprite_2d_toppled: Sprite2D = $Sprite2D_toppled
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready():
 	$HitBox.Damaged.connect( TakeDamage )
+	sprite_2d_toppled.visible = false
 	pass
 	
 func TakeDamage( _damage : int ) -> void:
-	queue_free()
+	topple()
 	pass
+	
+func topple() -> void:
+	animation_player.play("toppling")
+	animation_player.animation_finished.connect(_on_animation_finished)
+	
+func _on_animation_finished(name: String) -> void:
+	if name == "toppling":
+		sprite_2d.visible = false
+		sprite_2d_toppled.visible = true
