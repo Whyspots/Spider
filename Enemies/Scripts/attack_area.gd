ce968c16c4cb6f041b034f77d23fdb9aff8a5320
iokilo <jake672005@gmail.com>
2024-11-01 17:24:33 -0400

    fixed death screening

--- Enemies/Scripts/attack_area.gd
+++ Enemies/Scripts/attack_area.gd
@@ -1,11 +1,14 @@
 extends Area2D
 
+signal kill()
+
 func _ready() -> void:
 	body_entered.connect(_on_body_enter)
 	pass
 
 func _on_body_enter(_b : Node2D) -> void:
 	if (_b is Player):
-		LevelManager.load_new_level("res://gameover/gameover.tscn", Vector2.ZERO)
+		kill.emit()
+		
 	pass
 	
